5c2e60355ae1ac115c787561827449c1682b7c9e
syjkim0125 <syjkim0125@gmail.com>
2025-01-31 17:29:35 +0900

    add micro service options in main file

--- apps/user-grpc/src/main.ts
+++ apps/user-grpc/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+import { USER } from '@app/common';
+import { UserGrpcModule } from '@user-grpc/src/user-grpc.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UserGrpcModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        protoPath: join(__dirname, '../user.proto'),
+        package: USER,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
