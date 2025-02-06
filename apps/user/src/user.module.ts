f686f9b418176d23a0638d3cb82c782482ad0c01
syjkim0125 <syjkim0125@gmail.com>
2025-02-06 19:20:17 +0900

    apply global app guard in user http service

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { CqrsModule } from '@nestjs/cqrs';
+
 import { CommonModule } from '@common/common.module';
+import { JwtAuthGuard } from '@common/guards/jwt-auth.guard';
 import { UserCommonModule } from '@user-common';
 
 import { HttpControllers } from '@user/interface/controllers/http';
 
 import { QueryHandlers } from '@user/application/queries/handlers';
 import { ApplicationServices } from '@user/application/services';
+
+const globalAppGuards = [
+  {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  },
+];
 
 @Module({
   imports: [
@@ -15,6 +25,6 @@ import { ApplicationServices } from '@user/application/services';
     UserCommonModule,
   ],
   controllers: [...HttpControllers],
-  providers: [...QueryHandlers, ...ApplicationServices],
+  providers: [...globalAppGuards, ...QueryHandlers, ...ApplicationServices],
 })
 export class UserModule {}
